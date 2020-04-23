cc929eb70f31b2c4886ac49834e5c48546e4560f
Atarsei <2608822893@qq.com>
2020-04-23 12:58:20 +0800

    。

---
+++ blog_system.py
@@ -0,0 +1,9 @@
+# -*- coding:UTF-8 -*-
+from bs4 import BeautifulSoup
+import os
+def write_nav():
+    soup_bf = BeautifulSoup(open('d:/atarsei.github.io/#assets/parts/nav.html', encoding='utf-8'),features='html.parser')
+    soup=soup_bf.find_all('div')
+    filename = 'd:/atarsei.github.io/test_text.html'
+    with open(filename, 'w') as file_object:
+    file_object.write(str(soup[0]))
