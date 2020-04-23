c6d70db9e41a5b90e75abc4ffe337dd5b95610fa
Atarsei <2608822893@qq.com>
2020-04-23 16:08:50 +0800

    。

---
+++ Untitled-1.py
@@ -0,0 +1,6 @@
+import markdown
+input_file = open("d:atarsei.github.io/hhh.md", mode="r", encoding="utf-8")
+text = input_file.read()
+text.replace()
+print(text)
+print(markdown.markdown(text))
--- blog_system.py
+++ blog_system.py
@@ -4,6 +4,8 @@ import os
 def write_nav():
     soup_bf = BeautifulSoup(open('d:/atarsei.github.io/#assets/parts/nav.html', encoding='utf-8'),features='html.parser')
     soup=soup_bf.find_all('div')
-    filename = 'd:/atarsei.github.io/test_text.html'
+    return soup[0]
+def write(filename,text):    
+    #'d:/atarsei.github.io/test_text.html'
     with open(filename, 'w') as file_object:
-    file_object.write(str(soup[0]))
+        file_object.write(str(text))
