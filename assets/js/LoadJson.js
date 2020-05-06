52fbfe1a0d66ebf1040cba902087cd46f2767bd6
Atarsei <2608822893@qq.com>
2020-05-06 18:25:14 +0800

    pic for route

--- assets/js/LoadJson.js
+++ assets/js/LoadJson.js
@@ -1,8 +1,10 @@
 function LoadJsonWrite() 
 {
-    document.write("<br><br>");
+    document.write("<br><br><br>");
     var mydata = JSON.parse(data);
     for (var i = 0; i < mydata.length; i++) {
+        var pics ="";
+        if (mydata[i].p != ""){pics="<img src ="+mydata[i].p.split(',')[0]+"></img>"};
         if (mydata[i].c == "" && mydata[i].p == "") continue
         document.write(
             '<div class="route_card">\
@@ -10,12 +12,13 @@ function LoadJsonWrite()
             mydata[i].y + '.' + mydata[i].m + '.' + mydata[i].d +
             '</div>' +
             '<div class="route_card_content">' +
-            mydata[i].c +
+            mydata[i].c +pics+
             '</div>' +
             '<div class="route_card_bottom"></div>\
              </div>'
         );
     }
+    
 }
 
 function LoadJsonFrame()
