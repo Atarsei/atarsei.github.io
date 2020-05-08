fa4c086e92b596dabfda2f99fe1177af1507433c
Atarsei <2608822893@qq.com>
2020-05-08 11:06:17 +0800

    f a

--- Untitled-1.py
+++ Untitled-1.py
@@ -1,7 +1,9 @@
 # -*- coding:UTF-8 -*-
 import markdown
-input_file = open(file="D:/atarsei.github.io/hhh.md", mode="r", encoding="utf-8")
+input_file = open(file="D:/atarsei.github.io/assets/js/node_modules/marked/README.md", mode="r", encoding="utf-8")
 text = input_file.read()
 #text.replace()
-print(text)
-print(markdown.markdown(text))
+""" print(text)
+print(markdown.markdown(text)) """
+with open("readme.html",'w',encoding='utf-8')as file:
+    file.write(markdown.markdown(text,extensions=['extra']))
---
+++ assets/js/Load.js
@@ -0,0 +1,48 @@
+function LoadFile(filename)
+{
+    var file = new XMLHttpRequest();
+    file.open("GET",filename,false);
+    file.send();
+    return file.responseText;
+}
+
+function LoadJsonWrite(filename) 
+{
+    document.write("<br><br><br>");
+    var mydata = JSON.parse(LoadFile(filename));
+    for (var i = 0; i < mydata.length; i++) {
+        var pics ="";
+        if (mydata[i].p != ""){pics="<img src ="+'https://atarsei.gitee.io/assets/images/'+mydata[i].p.split(',')[0]+"></img>"};
+        if (mydata[i].c == "" && mydata[i].p == "") continue
+        document.write(
+            '<div class="route_card">\
+		     <div class="route_card_top">'+
+            mydata[i].y + '.' + mydata[i].m + '.' + mydata[i].d +
+            '</div>' +
+            '<div class="route_card_content">' +
+            mydata[i].c +pics+
+            '</div>' +
+            '<div class="route_card_bottom"></div>\
+             </div>'
+        );
+    }
+    
+}
+
+function LoadHtmlWrite(filename,classname)
+{
+    var outside = LoadFile(filename);
+    var outside_div =document.createElement('div');
+    outside_div.innerHTML =outside;
+    outside = outside_div.getElementsByClassName(classname)[0].outerHTML;
+    var inside = document.getElementsByClassName(classname)[0];
+    inside.outerHTML = outside;
+}
+function LoadFrame()/* Out Of Time   <iframe name="nav" src="../assets/Module/nav.html" style="display: none"></iframe>*/
+{
+    window.onload = function () {   
+        var nav = window.frames["nav"].document.getElementsByClassName('topmeau')[0].innerHTML;
+        var topmeau = document.getElementsByClassName('topmeau')[0];
+        topmeau.innerHTML = nav;
+      }
+}
