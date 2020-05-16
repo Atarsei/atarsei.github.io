4f1688b3f49d1aec855117248525cb0a6aa61b82
Atarsei <2608822893@qq.com>
2020-05-16 11:29:22 +0800

    长沙市

--- Untitled-1.py
+++
@@ -1,9 +0,0 @@
-# -*- coding:UTF-8 -*-
-import markdown
-input_file = open(file="D:/atarsei.github.io/assets/js/node_modules/marked/README.md", mode="r", encoding="utf-8")
-text = input_file.read()
-#text.replace()
-""" print(text)
-print(markdown.markdown(text)) """
-with open("readme.html",'w',encoding='utf-8')as file:
-    file.write(markdown.markdown(text,extensions=['extra']))
--- m.py
+++
@@ -1,7 +0,0 @@
-str =12
-def hhh(str):
-    j=1
-    str=str*2
-    return str,j
-
-print(hhh(str)[0])
